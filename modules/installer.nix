7e816f75146124a8ac0dc2ec86ade27768f50a63
Tomas Zaluckij <tz@tomaszal.com>
2023-12-12 08:49:09 +0000

    modules: add overrides management

--- modules/installer.nix
+++ modules/installer.nix
@@ -7,22 +7,53 @@ let
     else "\${XDG_STATE_HOME:-$HOME/.local/state}/home-manager/gcroots";
   stateFile = pkgs.writeText "flatpak-state.json" (builtins.toJSON {
     packages = map (builtins.getAttr "appId") cfg.packages;
+    overrides = cfg.overrides;
   });
   statePath = "${gcroots}/${stateFile.name}";
 
   updateApplications = cfg.update.onActivation || cfg.update.auto.enable;
   flatpakUninstallCmd = installation: {}: ''
-    # Can't uninstall if we don't know the old state.
-    if [[ -f ${statePath} ]]; then
-      # Uninstall all packages that are present in the old state but not the new one.
-      ${pkgs.jq}/bin/jq -r -n \
-        --slurpfile old ${statePath} \
-        --slurpfile new ${stateFile} \
-        '($old[].packages - $new[].packages)[]' \
-        | while read -r APP_ID; do
-            ${pkgs.flatpak}/bin/flatpak uninstall --${installation} -y $APP_ID
-          done
-    fi
+    # Uninstall all packages that are present in the old state but not the new one.
+    ${pkgs.jq}/bin/jq -r -n \
+      --argjson old "$OLD_STATE" \
+      --argjson new "$NEW_STATE" \
+      '($old.packages - $new.packages)[]' \
+      | while read -r APP_ID; do
+          ${pkgs.flatpak}/bin/flatpak uninstall --${installation} -y $APP_ID
+        done
+  '';
+
+  overridesDir =
+    if (installation == "system")
+    then "/var/lib/flatpak/overrides"
+    else "\${XDG_DATA_HOME:-$HOME/.local/share}/flatpak/overrides";
+  flatpakOverridesCmd = installation: {}: ''
+    # Update overrides that are managed by this module (both old and new)
+    ${pkgs.jq}/bin/jq -r -n \
+      --argjson old "$OLD_STATE" \
+      --argjson new "$NEW_STATE" \
+      '$new.overrides + $old.overrides | keys[]' \
+      | while read -r APP_ID; do
+          OVERRIDES_PATH=${overridesDir}/$APP_ID
+          
+          # Transform the INI-like Flatpak overrides file into a workable JSON
+          if [[ -f $OVERRIDES_PATH ]]; then
+            ACTIVE=$(cat $OVERRIDES_PATH \
+              | ${pkgs.jc}/bin/jc --ini \
+              | ${pkgs.jq}/bin/jq 'map_values(map_values(split(";") | select(. != []) // ""))')
+          else
+            ACTIVE={}
+          fi
+
+          # Generate and save the updated overrides file
+          ${pkgs.jq}/bin/jq -r -n \
+            --arg app_id "$APP_ID" \
+            --argjson active "$ACTIVE" \
+            --argjson old_state "$OLD_STATE" \
+            --argjson new_state "$NEW_STATE" \
+            --from-file ${./overrides.jq} \
+            >$OVERRIDES_PATH
+        done
   '';
 
   flatpakInstallCmd = installation: update: { appId, origin ? "flathub", commit ? null, ... }: ''
@@ -48,6 +79,14 @@ pkgs.writeShellScript "flatpak-managed-install" ''
   # This script is triggered at build time by a transient systemd unit.
   set -eu
 
+  # Setup state variables
+  NEW_STATE=$(cat ${stateFile})
+  if [[ -f ${statePath} ]]; then
+    OLD_STATE=$(cat ${statePath})
+  else
+    OLD_STATE={}
+  fi
+
   # Configure remotes
   ${mkFlatpakAddRemoteCmd installation cfg.remotes}
 
@@ -57,6 +96,9 @@ pkgs.writeShellScript "flatpak-managed-install" ''
 
   # Install packages
   ${mkFlatpakInstallCmd installation updateApplications cfg.packages}
+
+  # Configure overrides
+  ${flatpakOverridesCmd installation {}}
 
   # Save state
   ln -sf ${stateFile} ${statePath}
