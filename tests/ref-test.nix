5711e486774839ba871f6d528e5d8ad5f02d1ce5
Gabriele Modena <gmodena@pm.me>
2024-10-13 21:28:48 +0200

    test: remove duplicated code.

--- tests/ref-test.nix
+++ tests/ref-test.nix
@@ -8,6 +8,17 @@ let
   pwd = builtins.getEnv "PWD";
   fixturePath = "file://${pwd}/fixtures/package.flatpakref";
   fixtureHash = "040iig2yg2i28s5xc9cvp5syaaqq165idy3nhlpv8xn4f6zh4h1f";
+  expectedFixtureAttrSet = {
+    ${ref.sanitizeUrl fixturePath} = {
+      Title = "gedit";
+      Name = "org.gnome.gedit";
+      Branch = "stable";
+      Url = "http://sdk.gnome.org/repo-apps/";
+      IsRuntime = "false";
+      GPGKey = "REDACTED";
+      DeployCollectionID = "org.gnome.Apps";
+    };
+  };
 in
 runTests {
   testSanitizeUrl = {
@@ -47,31 +58,11 @@ runTests {
 
   testFlatpakrefToAttrSet = {
     expr = ref.flatpakrefToAttrSet { flatpakref = fixturePath; sha256 = null; } { };
-    expected = {
-      ${ref.sanitizeUrl fixturePath} = {
-        Title = "gedit";
-        Name = "org.gnome.gedit";
-        Branch = "stable";
-        Url = "http://sdk.gnome.org/repo-apps/";
-        IsRuntime = "false";
-        GPGKey = "REDACTED"; 
-        DeployCollectionID = "org.gnome.Apps";
-      };
-    };
+    expected = expectedFixtureAttrSet;
   };
 
   testFlatpakrefToAttrSetWithSha256 = {
     expr = ref.flatpakrefToAttrSet { flatpakref = fixturePath; sha256 = fixtureHash; } { };
-    expected = {
-      ${ref.sanitizeUrl fixturePath} = {
-        Title = "gedit";
-        Name = "org.gnome.gedit";
-        Branch = "stable";
-        Url = "http://sdk.gnome.org/repo-apps/";
-        IsRuntime = "false";
-        GPGKey = "REDACTED";
-        DeployCollectionID = "org.gnome.Apps";
-      };
-    };
+    expected = expectedFixtureAttrSet;
   };
 }
