dfb420aad3ac459f9377bdb137bc661ca89a74b5
Gabriele Modena <gm@nowave.it>
2024-10-20 21:14:50 +0200

    Revert "installer: forcefully delete remotes when managing state. (#86)"
    This reverts commit 849a15b46e1c139d9ec333ddd00e9088ae020a2e.

--- modules/installer.nix
+++ modules/installer.nix
@@ -173,16 +173,15 @@ let
   '';
   flatpakAddRemote = installation: remotes: map (flatpakAddRemotesCmd installation) remotes;
 
-  flatpakDeleteRemotesCmd = installation: uninstallUnmanagedState: {}: ''
+  flatpakDeleteRemotesCmd = installation: {}: ''
     # Delete all remotes that are present in the old state but not the new one
     # $OLD_STATE and $NEW_STATE are globals, declared in the output of pkgs.writeShellScript.
-    # If uninstallUnmanagedState is true, then the remotes will be deleted forcefully.
     ${pkgs.jq}/bin/jq -r -n \
       --argjson old "$OLD_STATE" \
       --argjson new "$NEW_STATE" \
        '(($old.remotes // []) - ($new.remotes // []))[]' \
       | while read -r REMOTE_NAME; do
-          ${pkgs.flatpak}/bin/flatpak remote-delete ${if uninstallUnmanagedState then " --force " else " " } --${installation} $REMOTE_NAME
+          ${pkgs.flatpak}/bin/flatpak remote-delete --${installation} $REMOTE_NAME
       done
   '';
 
@@ -216,7 +215,7 @@ pkgs.writeShellScript "flatpak-managed-install" ''
 
   # Uninstall remotes that have been removed from services.flatpak.packages
   # since the previous activation.
-  ${flatpakDeleteRemotesCmd installation uninstallUnmanagedState {}}
+  ${flatpakDeleteRemotesCmd installation {}}
 
   # Install packages
   ${mkFlatpakInstallCmd installation updateApplications cfg.packages}
