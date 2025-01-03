78ed84ff81e8d8510926e7165d508bcacef49ff1
Gabriele Modena <gmodena@pm.me>
2025-01-03 15:13:20 +0100

    installer: warning for manual app removal edge case (#130)
    * installer: warning for manual app removal edge case
    
    Don't hard fail on uninstall of packages already manually
    removed by the user.
    
    Display a warning message when an application is removed
    manually before nix-flatpak can manage it.

--- modules/installer.nix
+++ modules/installer.nix
@@ -108,7 +108,14 @@ let
       --argjson new "$NEW_STATE" \
       '(($old.packages // []) - ($new.packages // []))[]' \
     | while read -r APP_ID; do
-        ${pkgs.flatpak}/bin/flatpak uninstall --${installation} -y $APP_ID
+        # Guard against cases where a user removes an application both manually and through
+        # configuration between activations. This code path triggers when uninstallUnmanaged=false
+        # and nix-flatpak fails to clean up inconsistencies before reaching the uninstall phase.
+        if ${pkgs.flatpak}/bin/flatpak --${installation} list --app --columns=application | ${pkgs.gnugrep}/bin/grep -q "^$APP_ID$"; then
+          ${pkgs.flatpak}/bin/flatpak uninstall --${installation} -y $APP_ID
+        else
+          echo "WARNING: failed to uninstall '$APP_ID'. '$APP_ID' found in OLD_STATE, but not in '${installation}' installation. nix-flatpak state might be inconsistent."
+        fi
     done
   '';
 
