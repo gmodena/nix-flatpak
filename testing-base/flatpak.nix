53fc431d5db46b5427ca5573575bae258c6e0261
Gabriele Modena <gmodena@pm.me>
2025-02-11 22:48:03 +0100

    systemd: add service suffix to timer unit (#146)
    Update documentation and clean up testing-base usage
    examples.

--- testing-base/flatpak.nix
+++ testing-base/flatpak.nix
@@ -1,6 +1,7 @@
 # home.nix
 { lib, ... }: {
 
+  # Add a new remote. Keep the default one (flathub)
   services.flatpak.remotes = lib.mkOptionDefault [{
     name = "flathub-beta";
     location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
@@ -8,6 +9,8 @@
 
   services.flatpak.update.auto.enable = false;
   services.flatpak.uninstallUnmanaged = false;
+
+  # Add here the flatpaks you want to install
   services.flatpak.packages = [
     #{ appId = "com.brave.Browser"; origin = "flathub"; }
     #"com.obsproject.Studio"
