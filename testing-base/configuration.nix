1b280c47e2d34e7c0ed34cd0fcbe75d8bfe39d38
Gabriele Modena <gmodena@pm.me>
2026-01-17 14:30:39 +0100

    testing-base: version bump to 25.11
    Bump NixOS to 25.11 in test VMs.
    
    Signed-off-by: Gabriele Modena <gmodena@pm.me>

--- testing-base/configuration.nix
+++ testing-base/configuration.nix
@@ -30,8 +30,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Required to install flatpak
   xdg.portal = {
