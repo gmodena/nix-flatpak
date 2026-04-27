1c8e9fba70c148833fd986ab25f5895bbad5a7bb
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-04-27 15:49:26 +1000

    testing-base: add Hjem config

--- testing-base/flake.nix
+++ testing-base/flake.nix
@@ -4,9 +4,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hjem.url = "github:feel-co/hjem";
+    hjem.inputs.nixpkgs.follows = "nixpkgs";
+    hjem.inputs.nix-darwin.follows = "";
     flatpaks.url = "../";
   };
-  outputs = inputs@{ self, nixpkgs, home-manager, flatpaks, ... }:
+  outputs = inputs@{ nixpkgs, home-manager, hjem, flatpaks, ... }:
     let
       system = "x86_64-linux";
     in
@@ -37,6 +40,24 @@
             home-manager.users.antani.home.stateVersion = "23.11";
           }
           ./configuration.nix
+        ];
+      };
+
+      # hostname = test-hjem-module
+      nixosConfigurations.test-hjem-module = nixpkgs.lib.nixosSystem {
+        inherit system;
+        module = [
+          hjem.nixosModules.default
+          ./configuration.nix
+          {
+            config.hjem = {
+              extraModules = [
+                flatpaks.hjmeModules.flatpak
+                ./flatpak.nix
+              ];
+              users.alice.services.flatpak.enable = true;
+            };
+          }
         ];
       };
     };
