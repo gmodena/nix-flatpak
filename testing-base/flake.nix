7286725a93862245000dd65a3eff4a11f8ee3093
Gabriele Modena <gmodena@pm.me>
2026-06-08 10:31:18 +0200

    flake: fix hjem typos.
    Fix typos in hjem configuration.
    
    Signed-off-by: Gabriele Modena <gmodena@pm.me>

--- testing-base/flake.nix
+++ testing-base/flake.nix
@@ -1,7 +1,7 @@
 # flake.nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hjem.url = "github:feel-co/hjem";
@@ -9,56 +9,60 @@
     hjem.inputs.nix-darwin.follows = "";
     flatpaks.url = "../";
   };
-  outputs = inputs@{ nixpkgs, home-manager, hjem, flatpaks, ... }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      # hostname = test-system-module
-      nixosConfigurations.test-system-module = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          flatpaks.nixosModules.nix-flatpak
-          ./configuration.nix
-          ./flatpak.nix
-        ];
-      };
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    hjem,
+    flatpaks,
+    ...
+  }: let
+    system = "x86_64-linux";
+  in {
+    # hostname = test-system-module
+    nixosConfigurations.test-system-module = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        flatpaks.nixosModules.nix-flatpak
+        ./configuration.nix
+        ./flatpak.nix
+      ];
+    };
 
-      # hostname = test-hm-module
-      nixosConfigurations.test-hm-module = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs.flake-inputs = inputs;
-            home-manager.users."antani".imports = [
-              flatpaks.homeManagerModules.nix-flatpak
+    # hostname = test-hm-module
+    nixosConfigurations.test-hm-module = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs.flake-inputs = inputs;
+          home-manager.users."antani".imports = [
+            flatpaks.homeManagerModules.nix-flatpak
+            ./flatpak.nix
+          ];
+          home-manager.users.antani.home.stateVersion = "23.11";
+        }
+        ./configuration.nix
+      ];
+    };
+
+    # hostname = test-hjem-module
+    nixosConfigurations.test-hjem-module = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        hjem.nixosModules.default
+        ./configuration.nix
+        {
+          config.hjem = {
+            extraModules = [
+              flatpaks.hjemModules.nix-flatpak
               ./flatpak.nix
             ];
-            home-manager.users.antani.home.stateVersion = "23.11";
-          }
-          ./configuration.nix
-        ];
-      };
-
-      # hostname = test-hjem-module
-      nixosConfigurations.test-hjem-module = nixpkgs.lib.nixosSystem {
-        inherit system;
-        module = [
-          hjem.nixosModules.default
-          ./configuration.nix
-          {
-            config.hjem = {
-              extraModules = [
-                flatpaks.hjmeModules.flatpak
-                ./flatpak.nix
-              ];
-              users.alice.services.flatpak.enable = true;
-            };
-          }
-        ];
-      };
+            users.antani.services.flatpak.enable = true;
+          };
+        }
+      ];
     };
+  };
 }
